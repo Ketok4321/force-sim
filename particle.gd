4c369fed2c592d2ced600ffe7630897f868c4553
Ketok <ketok4321@gmail.com>
2025-01-26 21:18:07 +0100

    Make particles draggable, reduce proton mass

--- particle.gd
+++ particle.gd
@@ -12,12 +12,14 @@ const COLLISION_FORCE_STRENGTH = 1000
 @export var velocity = Vector2(0, 0)
 var force = Vector2(0, 0)
 
+var _drag_offset = null
+
 func _ready():
 	add_to_group("particle")
 
 func _process(delta: float) -> void:
 	force = Vector2(0, 0)
-	
+
 	var all_instances = get_tree().get_nodes_in_group("particle")
 	
 	for instance in all_instances:
@@ -27,7 +29,6 @@ func _process(delta: float) -> void:
 			if dist < DIST_CUTOFF * DIST_CUTOFF:
 				strength = max(strength, COLLISION_FORCE_STRENGTH)
 			force += (position - instance.position).normalized() * strength
-
 
 func _physics_process(delta: float) -> void:
 	velocity += force / mass * delta
@@ -41,6 +42,22 @@ func _physics_process(delta: float) -> void:
 	if position.y < 0: velocity.y = abs(velocity.y)
 	if position.y > vsize.y: velocity.y = -abs(velocity.y)
 
-
 func _draw():
 	draw_circle(Vector2(0, 0), radius, color)
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton:
+		var was_i_clicked = event.global_position.distance_squared_to(position) <= radius * radius
+		
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				if was_i_clicked:
+					_drag_offset = event.global_position
+				else:
+					_drag_offset = null
+			else:
+				if _drag_offset != null:
+					velocity += (event.global_position - _drag_offset) / 2
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			if event.pressed and was_i_clicked:
+				velocity = Vector2(0, 0)
