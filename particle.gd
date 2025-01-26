a052dc2abe740a94aa7a3d3430188c4bfaa809d2
Ketok <ketok4321@gmail.com>
2025-01-26 21:43:15 +0100

    Better controls

--- particle.gd
+++ particle.gd
@@ -12,8 +12,6 @@ const COLLISION_FORCE_STRENGTH = 1000
 @export var velocity = Vector2(0, 0)
 var force = Vector2(0, 0)
 
-var _drag_offset = null
-
 func _ready():
 	add_to_group("particle")
 
@@ -44,20 +42,3 @@ func _physics_process(delta: float) -> void:
 
 func _draw():
 	draw_circle(Vector2(0, 0), radius, color)
-
-func _input(event: InputEvent):
-	if event is InputEventMouseButton:
-		var was_i_clicked = event.global_position.distance_squared_to(position) <= radius * radius
-		
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				if was_i_clicked:
-					_drag_offset = event.global_position
-				else:
-					_drag_offset = null
-			else:
-				if _drag_offset != null:
-					velocity += (event.global_position - _drag_offset) / 2
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			if event.pressed and was_i_clicked:
-				velocity = Vector2(0, 0)
---
+++ scene.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@export var proton: PackedScene
+@export var electron: PackedScene
+
+var _drag_offset = null
+var _drag_particle = null
+
+func get_clicked(event):
+	for particle in get_tree().get_nodes_in_group("particle"):
+		if event.global_position.distance_squared_to(particle.position) <= particle.radius * particle.radius:
+			return particle
+	
+	return null
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var clicked = get_clicked(event)
+		
+		if clicked == null and event.pressed:
+			_drag_offset = null
+			_drag_particle = null
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				var new_scene = proton.instantiate()
+				new_scene.position = event.global_position
+				add_child(new_scene)
+			elif event.button_index == MOUSE_BUTTON_RIGHT:
+				var new_scene = electron.instantiate()
+				new_scene.position = event.global_position
+				add_child(new_scene)
+		elif event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				if clicked != null:
+					_drag_offset = event.global_position
+					_drag_particle = clicked
+			else:
+				if _drag_offset != null:
+					_drag_particle.velocity += (event.global_position - _drag_offset) / 2
+		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			clicked.velocity = Vector2(0, 0)
